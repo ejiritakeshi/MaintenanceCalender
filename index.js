e1464816f06a3f5c99604ac1b06a9458979b477f
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2022-11-03 21:22:44 +0900

    ログイン機能実装

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 window.onload = (e) => {
-  // Header
-  const header = new HeaderController();
-  header.createHeader();
-
-  // Input
-  const input = new Input();
-  const inputArea = input.createInputDOM();
-  const inputEl = document.getElementById("input");
-  inputEl.appendChild(inputArea);
-  Window.input = input;
-
-  // Calendar
-  const elem = document.getElementById("calendar");
-  const calendarController = new CalendarController();
-  const calendar = new FullCalendar.Calendar(elem, calendarController.configurationObject);
-  calendar.render();
-  
-  // Controller
-  calendarController.setCalendar(calendar);
-  Window.calendarController = calendarController;
-  
-  Window.dataController = new DataController();
-  Window.dataController.addAllEventsIntoCalendar();
-
-  header.setMenu();
+  const loginC = new LoginController();
+  loginC.createLoginScreen();
 };
