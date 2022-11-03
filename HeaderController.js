86f638c77c579f30e759175eb49dac6865991ac3
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2022-11-03 17:00:10 +0900

    ユーザー情報欄を作成

--- HeaderController.js
+++ HeaderController.js
@@ -6,7 +6,18 @@ class HeaderController {
     titleEl.innerText = "Maintenance Calendar";
     headerDom.append(titleEl);
 
-    
+    const userArea = document.createElement("div");
+    headerDom.append(userArea);
+
+    const usernameEl = document.createElement("h3");
+    const username = localStorage.getItem("username");
+    usernameEl.innerText = "こんにちは、" + username + "さん";
+    userArea.append(usernameEl);
+
+    const vehicleEl = document.createElement("div");
+    vehicleEl.innerText = "test vehicle";
+    userArea.append(vehicleEl);
+
     return headerDom;
   }
 }
